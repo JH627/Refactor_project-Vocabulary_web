4d213ac8929b1e5c5c6c4ab026086cf85a6cc506
Jung Hoon <52675944+JH627@users.noreply.github.com>
2024-03-08 00:42:56 +0900

    #1 (B/E 프로젝트 생성)
    백엔드 프로젝트 생성
    (Spring Web, Validation, JPA, Security, Querydsl, Lombok, MySQL 추가)

---
+++ voca_refactor/src/main/java/com/voca_refactor/VocaRefactorApplication.java
@@ -0,0 +1,13 @@
+package com.voca_refactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocaRefactorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocaRefactorApplication.class, args);
+	}
+
+}
---
+++ voca_refactor/src/test/java/com/voca_refactor/VocaRefactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voca_refactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocaRefactorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
